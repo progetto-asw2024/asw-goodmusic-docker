f8eeafccab18699f0c6c5514d1886c65f4b3b84e
SteMor1 <148955143+SteMor1@users.noreply.github.com>
2024-12-31 15:44:35 +0100

    Revert "Merge branch 'main' into main"
    This reverts commit 83b379555ea1c23c85a730293bf1710faf92d20b, reversing
    changes made to c9ef2f6d509a9bd682ce69c66ed38007db50cf88.

--- stop-goodmusic.sh
+++ stop-goodmusic.sh
@@ -2,5 +2,22 @@
 
 echo Halting GOODMUSIC
 
-docker compose down
+echo Stopping containers
 
+docker stop consul
+docker stop kafka
+docker stop $(docker ps -a | grep recensioni-seguite | awk '{print $1}')
+docker stop $(docker ps -a | grep recensioni | awk '{print $1}')
+docker stop $(docker ps -a | grep connessioni | awk '{print $1}')
+docker stop apigateway
+
+echo Removing containers
+
+docker rm consul
+docker rm kafka
+docker rm $(docker ps -a | grep recensioni-seguite | awk '{print $1}')
+docker rm $(docker ps -a | grep recensioni | awk '{print $1}')
+docker rm $(docker ps -a | grep connessioni | awk '{print $1}')
+docker rm apigateway
+
+docker network rm goodmusic-net
